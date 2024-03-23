aac3c067747b0aaa1e65da5fe55a26b425369088
Zeeshan Haider Soomro <zh7774440@gmail.com>
2024-03-24 01:27:02 +0500

    responsive-tasbeeh-counter

---
+++ index.js
@@ -0,0 +1,34 @@
+let value1 = document.getElementById("value1");
+let count1 = 0;
+
+function Inc1() {
+    count1++;
+    value1.innerText = count1;
+}
+function Dec1() {
+    count1 = 0
+    value1.innerText = count1;
+}
+let value2 = document.getElementById("value2");
+let count2 = 0;
+
+function Inc2() {
+    count2++;
+    value2.innerText = count2;
+}
+function Dec2() {
+    count2 = 0
+    value2.innerText = count2;
+}
+
+let value3 = document.getElementById("value3");
+let count3 = 0;
+
+function Inc3() {
+    count3++;
+    value3.innerText = count3;
+}
+function Dec3() {
+    count3 = 0
+    value3.innerText = count3;
+}
